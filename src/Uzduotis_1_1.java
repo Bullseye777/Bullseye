3ce467006baad012536e24fb7152483223a87be0
Bullseye777 <aurimux777@gmail.com>
2017-03-03 20:08:17 +0200

    Uzduotis 1 1

---
+++ src/Uzduotis_1_1.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+
+/**
+ * Created by A on 2017.03.03.
+ */
+public class Uzduotis_1_1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String vardas = sc.nextLine();
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+    }
+}
