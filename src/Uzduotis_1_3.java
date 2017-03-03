ef6cf1ecab60ff7d15079fe09de6645d7703aa26
Bullseye777 <aurimux777@gmail.com>
2017-03-03 20:27:02 +0200

    Uzduotis 1 3

---
+++ src/Uzduotis_1_3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by A on 2017.03.03.
+ */
+public class Uzduotis_1_3 {
+    public static void main(String[] args){
+        System.out.println("Sveiki, iveskite skaiciu a");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Iveskite skaiciu b");
+        int b = sc.nextInt();
+        int skirt = a-b;
+        int sum = a+b;
+        System.out.println("Skaiciu skirtumas yra : "+ skirt);
+        System.out.println("Skaiciu suma yra : "+ sum);
+    }
+}
