69befbe712846904f94d2cedc75b9667ecd63ce0
Bullseye777 <aurimux777@gmail.com>
2017-03-04 10:41:04 +0200

    Uzduotis 1 6

--- src/Uzduotis_1_6.java
+++ src/Uzduotis_1_6.java
@@ -8,11 +8,10 @@ public class Uzduotis_1_6 {
 System.out.println("Sveiki, iveskite savo mase, kg: ");
         Scanner sc = new Scanner(System.in);
         int mase = sc.nextInt();
-        System.out.println("Sveiki, iveskite savo ugi, m: ");
+        System.out.println("Iveskite savo ugi, m: ");
         float ugis = sc.nextFloat();
-        int i=2;
-        int ugis2 = (int) Math.pow(ugis, i);
+        int ugis2 = (int) Math.pow(ugis, 2);
         int kmi = mase/ugis2;
-        System.out.println("Sveiki, Jusu KMI: "+kmi);
+        System.out.println("Jusu KMI: "+kmi);
     }
 }
