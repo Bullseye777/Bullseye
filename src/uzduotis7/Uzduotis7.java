7b7514afef9272c680905d72b3799b783efa6503
Bullseye777 <aurimux777@gmail.com>
2017-03-07 17:06:30 +0200

    salygos sakiniai uzduotys

--- src/uzduotis7/Uzduotis7.java
+++ src/uzduotis7/Uzduotis7.java
@@ -8,16 +8,15 @@ import java.util.Scanner;
 public class Uzduotis7 {
     public Uzduotis7() {
         int sum = 0;
+        System.out.println("Įveskite skaičius kuriuos norite sudėti, pabaigti įveskite '0'");
         while (true) {
-            System.out.println("Įveskite skaičius kuriuos norite sudėti, pabaigti įveskite '0'");
             Scanner sc = new Scanner(System.in);
             int sk = sc.nextInt();
-
             if (sk != 0) {
                 sum = sum + sk;
 
             } else {
-                System.out.println("Įvestų skaičių sumą lygi:"+sum);
+                System.out.println("Įvestų skaičių suma lygi:" + sum);
             }
         }
     }
